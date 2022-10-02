8c23c5e20b123a3a4f19fe55167fd809a2576f15
Vojtech Mares <iam@vojtechmares.com>
2022-10-02 13:29:35 +0200

    fix(apps): postcss and tailwindcss configs

--- apps/r11y.news/tailwind.config.js
+++ apps/r11y.news/tailwind.config.js
@@ -1,10 +1,1 @@
-module.exports = {
-    content: [
-      '../../packages/ui/components/**/*.{ts,tsx}',
-      './src/**/*.{ts,tsx}'
-    ],
-    theme: {
-      extend: {}
-    },
-    plugins: []
-  }
+module.exports = require('config/tailwind.config')
