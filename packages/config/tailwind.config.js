b8a8ca908f599f8df22f2a94aaa588eeb5ccaa24
Vojtech Mares <iam@vojtechmares.com>
2022-10-03 09:42:24 +0200

    feat(packages/config): add ui package to tailwindcss paths

--- packages/config/tailwind.config.js
+++ packages/config/tailwind.config.js
@@ -1,7 +1,8 @@
 module.exports = {
     content: [
       '../../packages/ui/components/**/*.{ts,tsx}',
-      './src/**/*.{ts,tsx}'
+      './src/**/*.{ts,tsx}',
+      '../../packages/ui/**/*.{js,ts,jsx,tsx}',
     ],
     theme: {
       extend: {}
